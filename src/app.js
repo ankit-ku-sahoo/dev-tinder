f1089b6648e66fbd1288b9aa2ef1e66569bfca8c
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2024-10-14 21:35:44 +0530

    Working of middlewares

--- src/app.js
+++ src/app.js
@@ -2,34 +2,45 @@ const express = require("express")
 
 const app = express()
 
-// This GET call will work for "abc" as well as "ac", basically anything before ? is optional
-app.get("/ab?c", (req,res) => {
-    res.send("Working with ? ")
-});
+app.use(
+    "/user", 
+    (req,res) => {
+        console.log("Inside /user")
+        res.send("Handling route /user... ")
+    }
+)
 
-// This GET call will work for "abc", "abbc" and so on...
-app.get("/(ab)+c", (req,res) => {
-    res.send("Working with + ")
-});
+// This will go into an infinite loop & will timeout as there's no response sent
+app.use(
+    "/test1", 
+    (req,res) => {
+        console.log("Inside /user")
+        // res.send("Handling route /user... ")
+    },
+    (req,res) => {
+        console.log("Inside /user PART 2")
+        res.send("Handling route /user... ")
+    },
+)
 
-// This GET call will work for "ab" + {anything} + "c"
-app.get("/ab*c", (req,res) => {
-    res.send("Working with * ")
-});
-
-app.get("/user/:userID/:name", (req,res) => {
-    console.log(req.params)
-    res.send("Working with * ")
-});
-
-app.get("/user", (req,res) => {
-    console.log(req.query)
-    res.send("Working with * ")
-});
-
-app.use((req,res) => {
-    res.send("Hello from the server!")
-});
+// This will send a response as the next handler was called
+app.all(
+    "/test2", 
+    (req,res,next) => {
+        console.log("Inside /user")
+        next()
+        // res.send("this should give me an error")
+    },
+    (req,res,next) => {
+        console.log("Inside /user PART 2")
+        next()
+        // res.send("Handling route /user... ")
+    },
+    (req,res) => {
+        console.log("Inside /user PART 3")
+        res.send("Handling route /user... ")
+    },
+)
 
 app.listen(3001, () => {
     console.log("Server is successfully listening on port 3001... ")
