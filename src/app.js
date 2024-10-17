ad8a9ff790aae1ca3e20714f0ac63d8d4a282296
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2024-10-17 21:58:43 +0530

    Created GET, POST, PATCH APIs to update datain DB

--- src/app.js
+++ src/app.js
@@ -4,13 +4,10 @@ const User = require("./models/user")
 
 const app = express()
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
-    const userObj = {
-        firstName: "Ankit Kumar",
-        lastName: "Sahoo",
-        emailId: "10ankitkusahoo10@gmail.com",
-        password: "abcd1234"
-    }
+    const userObj = req.body
 
     const user = new User(userObj)
 
@@ -22,6 +19,71 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("Error saving the user...")
     }
     
+})
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId
+
+    try {
+        const user = await User.findOne({ emailId: userEmail})
+        if(user.length === 0) {
+            res.status(404).send("No user found")
+        }
+        else{
+            res.send(user)
+        }
+        
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId
+    
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+
+})
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId
+    const updatedUserDetails = {...req.body, "_id": req.body.userId}
+    
+    try{
+        const user = await User.findByIdAndUpdate(userId, updatedUserDetails, {returnDocument: "after"});
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.patch("/user/:emailId", async (req, res) => {
+    const emailId = req.params.emailId
+    const updatedUserDetails = req.body
+
+    console.log(emailId)
+
+
+    try{
+        const user = await User.findOneAndUpdate({ emailId: emailId }, updatedUserDetails, {returnDocument: "after"});
+        if(user.length === 0){
+            res.status(404).send("User not found")
+        }
+        else{
+            res.send(user)
+        }
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
 })
 
 connectDB()
