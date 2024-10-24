566ef752bec9f26dcaecbb514349728019736dc4
Ankit Kumar Sahoo <10ankitkusahoo10@gmail.com>
2024-10-24 20:45:34 +0530

    Encrypted passwords using bcrypt

---
+++ src/utils/validation.js
@@ -0,0 +1,15 @@
+const validator = require("validator")
+
+const validateUserSignups = (req) => {
+    const {firstName, lastName, emailId, password } = req.body
+
+    if(!firstName){
+        throw new Error("Please enter a first name")
+    } else if (!lastName) {
+        throw new Error("Please enter a last name")
+    } else if (!validator.isStrongPassword(password)) {
+        throw new Error("Please enter a strong password!")
+    }
+}
+
+module.exports = { validateUserSignups }
